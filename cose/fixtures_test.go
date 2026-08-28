9abc9166f052690ff788fb26e343dd0d1a7bd98e
Péter Szilágyi <peterke@gmail.com>
2026-08-28 11:12:35 +0300

    cose, cwt, stream: fix source file year

--- cose/fixtures_test.go
+++ cose/fixtures_test.go
@@ -1,5 +1,5 @@
 // crypto-go: cryptography primitives and wrappers
-// Copyright 2025 Dark Bio AG. All rights reserved.
+// Copyright 2026 Dark Bio AG. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
